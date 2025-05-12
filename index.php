001c7271e6fa0db76bbf0ee85c3be4585d691aa5
cexs <cesar.serafin@outlook.com>
2025-05-12 09:43:48 -0600

    index actualizado

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
-require 'vendor/autoload.php';
+
+require __DIR__ . '/vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Controllers\añadirLibroCtrl;
 
 $app = AppFactory::create();
+$app->setBasePath('/Servicio-Php');
 
-// Cargar rutas
-(require __DIR__ . '/routes/añadirLibro.php')($app);
+// Ruta para añadir un libro
+$app->post('/AñadirLibro', function (Request $request, Response $response) {
+    $controller = new añadirLibroCtrl();
+    return $controller->añadirLibro($request, $response, []);
+});
 
 $app->run();
